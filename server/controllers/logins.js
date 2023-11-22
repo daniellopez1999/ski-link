7eaa8cdf8b83cf1379133cc91126883ddd892509
Daniel López Bogaz <lopezbogazdaniel@gmail.com>
2023-11-22 11:05:53 +0100

    backend tests done

--- server/controllers/logins.js
+++ server/controllers/logins.js
@@ -20,7 +20,7 @@ exports.postLogin = async (req, res) => {
         res.status(401).json({ message: 'Invalid credentials' });
       }
     } else {
-      res.status(401).json({ message: 'Invalid credentials' });
+      res.status(402).json({ message: 'Invalid credentials' });
     }
   } catch (error) {
     res.status(500).json({ message: 'Internal server error' });
@@ -34,7 +34,7 @@ exports.registerUser = async (req, res) => {
     const userExists = await User.findOne({ email });
 
     if (userExists) {
-      res.status(401).json({ message: 'User already exists' });
+      res.status(403).json({ message: 'User already exists' });
     } else {
       const passwordHashed = await bcrypt.hash(password, 10);
       const user = await User.create({
---
+++ server/tests/jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/tests/setupFile.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  clearMocks: true,
+  injectGlobals: false,
+  collectCoverage: true,
+  coverageReporters: process.env.CI ? ["text", "lcov"] : ["text"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
